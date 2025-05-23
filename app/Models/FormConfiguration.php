4d39a99622cdc7d3677de63f9c63d5a4fb34d041
Handrisza Arya Resta <handriszaaryar@gmail.com>
2025-05-23 16:22:42 +0700

    add eloquent

--- app/Models/FormConfiguration.php
+++ app/Models/FormConfiguration.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class FormConfiguration extends Model
 {
-    //
+    protected  $guarded = [];
+
+    protected $casts = [
+        'payloads' => 'array',
+        'is_active' => 'boolean'
+    ];
+
+    public function submissions()
+    {
+        return $this->hasMany(FormSubmission::class, 'form_id', 'form_id');
+    }
+
+    public function scopeActive($query)
+    {
+        return $query->where('is_active', true);
+    }
+
 }
--- app/Models/FormSubmission.php
+++ app/Models/FormSubmission.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class FormSubmission extends Model
 {
-    //
+    protected $guarded = [];
+    protected $casts = [
+        'submission_data' => 'array',
+        'submitted_at' => 'datetime'
+    ];
+
+    public function formConfiguration()
+    {
+        return $this->belongsTo(FormConfiguration::class, 'form_id', 'form_id');
+    }
+
+    public function scopeByStatus($query, $status)
+    {
+        return $query->where('status', $status);
+    }
 }
